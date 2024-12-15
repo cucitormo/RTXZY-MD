e4307330329361c30ff01e6b92c771156d1f6d7e
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-12-15 21:41:34 +0700

    Update _autoresetlimit.js

--- plugins/_autoresetlimit.js
+++ plugins/_autoresetlimit.js
@@ -6,7 +6,7 @@ module.exports = {
     if (current.getHours() === 0 && current.getMinutes() === 0) {
       let list = Object.entries(global.db.data.users);
       list.forEach(([user, data]) => {
-        data.limit = 100;
+        data.limit = 100; //sesuaikan dengan limit yang dibutuhkan
       });
       
       console.log('Limit user berhasil direset pada', current);
