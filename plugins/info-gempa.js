f46e467db3eb2d390ee57d50165bcd22cafd27df
BOTCAHX <94074076+BOTCAHX@users.noreply.github.com>
2024-12-21 10:36:39 +0700

    Update info-gempa.js

--- plugins/info-gempa.js
+++ plugins/info-gempa.js
@@ -1,20 +1,10 @@
 var axios = require('axios');
 var handler = async (m, { conn }) => {
 try {
-var dataGempa = [];
   var response = await axios.get(`https://api.botcahx.eu.org/api/search/gempa?apikey=${btc}`);
-  var res = response.data;
-  var gambar = res.result.Map;
-  dataGempa.push({
-    waktu: res.result.Waktu,
-    lintang: res.result.Lintang,
-    bujur: res.result.Bujur,
-    magnitude: res.result.Magnitudo,
-    kedalaman: res.result.Kedalaman,
-    wilayah: res.result.Wilayah
-  });
-  var caption = `Waktu : ${dataGempa[0].waktu}\nLintang : ${dataGempa[0].lintang}\nBujur : ${dataGempa[0].bujur}\nMagnitude : ${dataGempa[0].magnitude}\nKedalaman : ${dataGempa[0].kedalaman}\nWilayah : ${dataGempa[0].wilayah}`;
-  conn.sendFile(m.chat, gambar, 'map.png', caption, m);
+  var dataGempa = response.data.result.result;
+  var caption = `Waktu : ${dataGempa.waktu}\nLintang : ${dataGempa.Lintang}\nBujur : ${dataGempa.Bujur}\nMagnitude : ${dataGempa.Magnitudo}\nKedalaman : ${dataGempa.Kedalaman}\nWilayah : ${dataGempa.Wilayah}`;
+  conn.sendFile(m.chat, dataGempa.image, 'map.png', caption, m);
 } catch(e) {
   console.log(e);
   conn.reply(m.chat, 'Terjadi kesalahan saat mengambil data gempa', m);
